9bd2b0446548d000e2867e4f083bcc7ba5a94366
Tibi <tvoinea@my.bcit.com>
2022-11-12 20:21:43 -0800

    added the functionality to add the new course to the list

--- script/javascript.js
+++ script/javascript.js
@@ -3,3 +3,25 @@ const courseList = [
     { code: "ACIT 1310", name: " Technical math for IT" },
     { code: "ACIT 1620", name: " Web Fundamentals" }
 ];
+
+let course = prompt("Enter a 4 digit number: ");
+
+while (course.lenght != 4) {
+    console.log("Invalid course code");
+    course = prompt("Enter a 4 digit number:");
+}
+
+let found = false;
+
+for (let i = 0; i < courseList.length; i++) {
+    if (courseList[i].code.includes(course)) {
+        found = true;
+        console.log(`Yes I am taking the course: ${course} - ${courseList[i].name}`);
+    }
+
+}
+
+if (!found) {
+    courseList.push({ code: course, name: null });
+    console.log("Added new course to the list");
+}
