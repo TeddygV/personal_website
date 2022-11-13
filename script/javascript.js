b6f1c8c283cadee8b9a391ba1b65f30ac3bdead5
Tibi <tvoinea@my.bcit.com>
2022-11-12 20:09:39 -0800

    added courselist in java

---
+++ script/javascript.js
@@ -0,0 +1,5 @@
+const courseList = [
+    { code: "ACIT 1515", name: " Scripting for IT" },
+    { code: "ACIT 1310", name: " Technical math for IT" },
+    { code: "ACIT 1620", name: " Web Fundamentals" }
+];
